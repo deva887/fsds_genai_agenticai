792e62bd44bba6b98f93f1fcc9f4d660ce65c2e7
deva887 <devakumarsattala@gmail.com>
2025-06-19 20:39:33 +0530

    Add files via upload

---
+++ work_numbers.py
@@ -0,0 +1,19 @@
+# hashtag is used to make a line as comment
+# addition of 2 numbers
+a=20  #20 is assigned to a variable called a
+b=5   #5 is assigned to a variable called b
+print(a+b)#addtion
+print(a-b)#subtraction
+print(a*b)#multiplication
+print(a/b)#division with floating point or decimal point
+print(a//b)#floor division will give you the int result
+print(a%b)#reminder
+print((50-6*4)-4*5)# follows bodmas rule
+print(5**2)# means 5^2
+print(3**2)# ** operator is used to calculate the powers 3^2
+l=20# the equal(=) operator is used to assign  value to a variable.And it does not display any value until you ask for it
+b=40
+area=l*b
+print(area)
+# note:the int values were always turned to float values to maintain precision
+# the last printed value is assigned to underscore(_) only for rep(read evaluate print loop)works only in online editors
