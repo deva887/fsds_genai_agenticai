5e12f47f96ad06289dd4601881efc34e08fcf03f
deva887 <devakumarsattala@gmail.com>
2025-06-21 09:15:10 +0530

    Add files via upload
    slicing and indexing

---
+++ day3.py
@@ -0,0 +1,24 @@
+#strings-20-06-2025
+#a text is called a string
+p=20
+q=20
+r=20
+a=b=c=10#this is code optimization
+print(id(a))
+print(id(b))
+print(id(c))
+x,y,z=10,20,30
+print(id(z))
+s='hello python'
+print(len(s))
+print(s[0])#forward indexing
+print(s[-1])#backward indexing
+print(s[11])
+print(s[0:5])#slicing
+print(s[6:9])
+print(s[-5:-1])
+print(s[1:])#from 1 to till last
+print(s[:5])#from start till 5-1
+#advance slicing
+print(s[::-1])#reverse
+print(s[0:12:2])#stepping.....(np.nan)
