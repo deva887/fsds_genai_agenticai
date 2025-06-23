1cf87e796ea88549c34554bb2f0a8d3daf9538a4
deva887 <devakumarsattala@gmail.com>
2025-06-23 09:10:26 +0530

    Add files via upload

---
+++ type_conversion.py
@@ -0,0 +1,40 @@
+#type conversions from any to int
+print(int(2.9))#float to int
+print(int(2))#int to int
+print(int('2'))#string to int only applicable for numercical strings
+print(int(True))#bool to int
+print(int(False))#bool to int
+print(int('100'))#string to int
+# print(int(3+2j)) not possible to convert complex to int-------------------------------------------------
+#type conversion from any to float
+print(float(2))
+print(float(2.2))
+print(float(True))
+print(float('2'))
+print(float())
+# print(float(3+3j))#not possible to convert complex to float------------------------------------------
+#type conversion from any to bool
+print(bool(0))
+print(bool(1))
+print(bool(2.32))
+print(bool(True))
+print(bool(False))
+print(bool(2+3j))
+print(bool("324"))
+print(bool("sdfie"))# so any thing to bool is possible------------------------
+#type conversion from any to complex
+print(complex(100))
+print(complex(1))
+print(complex(0))
+print(complex(4.0))
+print(complex(4.2))
+print(complex(True))
+print(complex(False))
+print(complex('1'))#so any to complex is possible but except the string with alphabet--'example'
+
+
+
+
+
+
+
